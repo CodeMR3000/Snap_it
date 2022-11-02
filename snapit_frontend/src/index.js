f83e262feb523866ca9f85170a3be268bd539d1b
Manish <manishraj3322@gmail.com>
2022-11-03 02:18:06 +0530

    initial commit

---
+++ snapit_frontend/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <Router>
+    <App />
+   </Router>
+  ,document.getElementById('root'),
+);
+
+
+
+// import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('root');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(
+// <Router>
+//   <App />
+// </Router>
+
+// );
